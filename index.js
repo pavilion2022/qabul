3123902412a78bfc07178ea9a56f9a1e58cae97b
pavilion2022 <muhammadali-2008@mail.ru>
2022-07-27 17:41:30 +0500

    html2 and index.html, index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+Number
+String
+Boolean
+null
+undefined
+Array
+Object
+
+var parol= 12345
+var a =20
+var b =20
+a=55
+
+
+console.log(a+b)
+/*alert ('Salom')*/
+/*confirm ('xato')*/
+prompt ('Ismingiz', 'react')
